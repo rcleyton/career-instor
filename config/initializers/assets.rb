5561127e553781d23c285953bf7245ba4cbd4c6a
Cleyton Silva <silva.rcleyton@gmail.com>
2022-09-03 17:37:08 -0300

    Apply theme to dashboard

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( dashboard.js dashboard.css )
+
+Rails.application.config.assets.precompile += %w( custom.js custom.css instor.png )
+
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
