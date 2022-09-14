33091ccfc9f1572f8651e16b125894d40601e031
Cleyton Silva <silva.rcleyton@gmail.com>
2022-09-14 17:47:20 -0300

    redirect to dashboard after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resourece
 
+  def after_sign_in_path_for(resource)
+    dashboard_home_index_path
+  end
+
   private
 
   def layout_by_resourece
